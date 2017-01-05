748b010e3d802fb2f89a63752ac5209a7afacbe4
Christopher Bennage <bennage@microsoft.com>
2017-01-05 15:54:22 -0800

    generating a series pages

--- docs/_build/make.js
+++ docs/_build/make.js
@@ -4,20 +4,47 @@ var fs = require('fs');
 var yamlFront = require('yaml-front-matter')
 var Liquid = require('shopify-liquid');
 
+var listFiles = require('./listFiles.js');
+
 var engine = Liquid({
     root: path.resolve(__dirname, '.'),
     extname: '.liquid'
 });
 
-var inputFilePath = '../app-service/index.liquid.md';
-var outputFilePath = '../app-service/index.md'
+var pathToDocs = '..';
 
-var content = fs.readFileSync(inputFilePath, 'utf8');
-var yml = yamlFront.loadFront(content);
-
-var template = engine.parse(content);
-
-engine.render(template, yml)
-    .then(function (html) {
-        fs.writeFile(outputFilePath, html)
+listFiles({ cwd: pathToDocs, pattern: '**/*.liquid.md' })
+    .then(x => {
+        var renderings = x.files.map(f => {
+            return render(path.resolve(pathToDocs, f));
+        }
+        );
+        return Promise.all(renderings);
+    })
+    .then(() => {
+        console.log('Done.');
     });
+
+
+function render(inputFilePath) {
+    console.log(inputFilePath);
+    var outputFilePath = inputFilePath.replace('.liquid', '');
+
+    var content = fs.readFileSync(inputFilePath, 'utf8');
+    var yml = yamlFront.loadFront(content);
+
+    // hack
+    // this next part is necessary because OPS doesn't allow object arrays in the yml
+    var frontmatter = Object.assign({}, yml);
+    delete frontmatter.articles
+    delete frontmatter.__content
+    content = '---\n' + yamlFront.dump(frontmatter) + '---' + yml.__content;
+    // end of hack
+
+    var template = engine.parse(content);
+
+    return engine.render(template, yml)
+        .then(function (markdown) {
+            fs.writeFile(outputFilePath, markdown)
+        });
+}
