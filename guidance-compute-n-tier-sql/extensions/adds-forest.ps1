80492c50908a343f8c1b577fad2253c0800ff59a
Michael Pritchard <mpritchard@netchexonline.com>
2017-01-06 17:15:48 -0600

    Updated to remove requirement for secondary data drive

--- guidance-compute-n-tier-sql/extensions/adds-forest.ps1
+++ guidance-compute-n-tier-sql/extensions/adds-forest.ps1
@@ -7,10 +7,6 @@ Param(
 
 $ErrorActionPreference = "Stop"
 
-Initialize-Disk -Number 2 -PartitionStyle GPT
-New-Partition -UseMaximumSize -DriveLetter F -DiskNumber 2
-Format-Volume -DriveLetter F -Confirm:$false -FileSystem NTFS -force 
-
 Install-windowsfeature -name AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
 
 Import-Module ADDSDeployment
@@ -20,13 +16,10 @@ $secSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Fo
 Install-ADDSForest `
 -SafeModeAdministratorPassword $secSafeModePassword `
 -CreateDnsDelegation:$false `
--DatabasePath "F:\Windows\NTDS" `
 -DomainMode "Win2012R2" `
 -DomainName $DomainName `
 -DomainNetbiosName $DomainNetbiosName `
 -ForestMode "Win2012R2" `
 -InstallDns:$true `
--LogPath "F:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
--SysvolPath "F:\Windows\SYSVOL" `
 -Force:$true
